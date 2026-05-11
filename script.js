5e98f3b91e446dd16f49361b848046a042f10a24
jalla <jallasimhadri@gmail.com>
2026-05-11 11:27:54 +0530

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+/* CLOSE MENU WHEN CLICKING LINKS */
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+
+});
+
+
+ 
--- scrpt.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector(".nav-links");
-
-menuBtn.onclick = () => {
-  navLinks.classList.toggle("active");
-};
